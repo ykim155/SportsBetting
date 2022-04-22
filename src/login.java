e95e77c3d42bbfd632302df21f303c6da8b412cc
Yujin Kim - PC <yujin.eugene.kim@gmail.com>
2022-04-22 12:07:35 -0400

    fixed clock names

--- src/login.java
+++ src/login.java
@@ -170,7 +170,7 @@ public class login extends JFrame{
 					   Date  date = new Date();
 					   String str = String.format("%tc", date);
 	              	   
-					   String titleString = "--- Food Truck Kiosk --- " + str; 				 
+					   String titleString = "--- Login --- " + str; 				 
 					   
 					   setTitle(titleString);
 						 
